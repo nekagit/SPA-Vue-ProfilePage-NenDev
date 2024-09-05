9aec925ca19efd5a9c50199b120fb3cd97775740
nekagit <nenad99@hotmail.de>
2024-09-05 15:07:39 +0200

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
+<!-- App.vue or your main layout component -->
 <template>
   <div>
+    <LoadingScreen />
     <NavBar />
     <router-view class="router bg-white dark:bg-gray-900 text-black dark:text-white" />
     <NenFooter />
@@ -7,37 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import NavBar from './components/NenDev/NavBar.vue';
-import NenFooter from './components/NenDev/NenFooter.vue';
-
-// const isLoading = ref(false);
-// const showVideo = ref(false);
-
-// const onVideoEnded = () => {
-//   showVideo.value = false;
-// };
-
-// router.beforeEach((to, from, next) => {
-//   isLoading.value = true;
-//   next();
-// });
-
-// router.afterEach(() => {
-//   setTimeout(() => {
-//     isLoading.value = false;
-//   }, 3000);
-// });
-
-onMounted(() => {
-  // console.log("Video source:", video);localStorage.removeItem('videoPlayed');
-  // // Check if the video has been played before
-  // if (!localStorage.getItem('videoPlayed')) {
-  //   console.log("Video should play");
-  //   showVideo.value = true;
-  //   localStorage.setItem('videoPlayed', 'true');
-  // } else {
-  //   console.log("Video has been played before");
-  // }
-});
+import NenFooter from '@/components/NenDev/NenFooter.vue';
+import LoadingScreen from '@/components/LoadingScreen.vue';
 </script>
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,52 @@
+<!-- components/LoadingScreen.vue -->
+<template>
+  <div v-if="isVisible" :class="{'loading-screen': true, 'fade-out': !isVisible}">
+    <img src="./../../public/N.png" alt="Logo" class="logo" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const isVisible = ref(true);
+
+onMounted(() => {
+  // Simulate a delay before hiding the loading screen
+  setTimeout(() => {
+    isVisible.value = false;
+  }, 3000); // Adjust the delay time as needed
+});
+</script>
+
+<style scoped>
+.loading-screen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+  opacity: 1;
+  transition: opacity 1s ease-in-out; /* Smooth fade-in and fade-out */
+}
+
+.fade-out {
+  opacity: 0;
+}
+
+.logo {
+  width: 300px; /* Adjust size as needed */
+  height: auto;
+  animation: blink 1s infinite;
+}
+
+@keyframes blink {
+  0% { opacity: 1; }
+  50% { opacity: 0.5; }
+  100% { opacity: 1; }
+}
+</style>
