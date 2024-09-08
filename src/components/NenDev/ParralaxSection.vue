6be9f716b1b10a20c9e8151d3643e39a80258b4f
nekagit <nenad99@hotmail.de>
2024-09-08 20:55:14 +0200

    asdfasdf

--- src/components/NenDev/ParralaxSection.vue
+++ src/components/NenDev/ParralaxSection.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
+
 interface IProps {
   imgSrc: string
 }
 
 defineProps<IProps>()
+
 const parallaxContainer = ref<HTMLElement | null>(null)
+const parallaxImage = ref<HTMLElement | null>(null)
 
 const handleScroll = () => {
-  if (parallaxContainer.value) {
+  if (parallaxContainer.value && parallaxImage.value) {
     const scrollPosition = window.pageYOffset
     const containerRect = parallaxContainer.value.getBoundingClientRect()
     const containerTop = containerRect.top + scrollPosition
     const containerBottom = containerTop + containerRect.height
-
     if (scrollPosition > containerTop - window.innerHeight && scrollPosition < containerBottom) {
-      const parallaxImage = parallaxContainer.value.querySelector('.parallax-image')
       const parallaxSpeed = 0.5 // Adjust this value to change the parallax speed
       const yOffset = (scrollPosition - containerTop) * parallaxSpeed
-      parallaxImage.style.transform = `translateY(${yOffset}px)`
+      parallaxImage.value.style.transform = `translateY(${yOffset}px)`
     }
   }
 }
 
 onMounted(() => {
   window.addEventListener('scroll', handleScroll)
-  // Smooth scroll for anchor links
 })
 
 onUnmounted(() => {
@@ -34,13 +34,10 @@ onUnmounted(() => {
 </script>
 
 <template>
-
-  <div class="parallax-container my-16" id="parallax">
-    <div class="parallax-image" :style="{ backgroundImage: `url(${imgSrc})` }"></div>
+  <div ref="parallaxContainer" class="parallax-container my-16" id="parallax">
+    <div ref="parallaxImage" class="parallax-image" :style="{ backgroundImage: `url(${imgSrc})` }"></div>
   </div>
-
 </template>
-
 
 <style scoped>
 .parallax-container {
@@ -48,6 +45,7 @@ onUnmounted(() => {
   height: 60vh;
   overflow: hidden;
 }
+
 .parallax-image {
   position: absolute;
   top: 0;
@@ -58,6 +56,4 @@ onUnmounted(() => {
   background-position: center;
   background-attachment: fixed;
 }
-
-
 </style>
