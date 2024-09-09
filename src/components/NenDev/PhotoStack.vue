1e27ed95108434af00accab5985cba0d16cce6c4
nekagit <nenad99@hotmail.de>
2024-09-09 13:52:00 +0200

    asdf

--- src/components/NenDev/PhotoStack.vue
+++ src/components/NenDev/PhotoStack.vue
@@ -1,59 +1,67 @@
 <template>
-    <div class="relative">
-
-  <div class="flex flex-col md:flex-row items-center justify-center text-white p-4 container">
-    <div class="w-full md:w-1/2 md:pr-8">
-   
-      <div class="relative w-full h-96 mb-8">
-        <TransitionGroup name="photo-stack">
-          <div
-            v-for="(photo, index) in visiblePhotos"
-            :key="photo.src"
-            class="absolute top-0 left-0 w-full h-full"
-            :style="{
-              transform: `rotate(${getRandomRotation(index)}deg)`,
-              zIndex: visiblePhotos.length - index
-            }"
-            @click="nextPhoto"
-          >
-            <img
-              :src="photo.src"
-              :alt="photo.alt"
-              class="w-full h-full object-cover rounded-lg shadow-lg cursor-pointer slide-in"
-            />
-          </div>
-        </TransitionGroup>
-      </div>
-    </div>
-    <div class="w-full md:w-[400px]">
-      <div class="flex justify-between mb-8">
-        <div v-for="(value, key) in stats" :key="key" class="text-center">
-          <span class="text-blue-400 text-3xl block">{{ value }}</span>
-          <span class="text-sm">{{ formatKey(key) }}</span>
+  <div class="relative">
+    <div class="flex flex-col md:flex-row items-center justify-center text-white p-4 container">
+      <div class="w-full md:w-1/2 md:pr-8">
+        <div class="relative w-full h-96 mb-8">
+          <TransitionGroup name="photo-stack">
+            <div
+              v-for="(photo, index) in visiblePhotos"
+              :key="photo.src"
+              class="absolute top-0 left-0 w-full h-full"
+              :style="{
+                transform: `rotate(${getRandomRotation(index)}deg)`,
+                zIndex: visiblePhotos.length - index
+              }"
+              @click="nextPhoto"
+            >
+              <img
+                :src="photo.src"
+                :alt="photo.alt"
+                class="w-full h-full object-cover rounded-lg shadow-lg cursor-pointer slide-in"
+              />
+            </div>
+          </TransitionGroup>
         </div>
       </div>
-      <div class="space-y-4">
-        <p>
-          I'm a Cape Town based Web Developer and Entrepreneur. My focus area for the past few years
-          has been front-end development with Next.js, Typescript and TailwindCSS to create
-          beautiful user- and developer experiences that bring delight.
-        </p>
-        <p>
-          I've spent most of my life deeply interested in technology and food, continuously building
-          things with both. As a teenager, I was a classic computer nerd, spending most of my time
-          modding games, tinkering with hardware, and developing websites.
-        </p>
+      <div class="w-full md:w-[400px] mt-8 mx-8">
+        <div class="flex justify-between mb-8">
+          <div v-for="(value, key) in stats" :key="key" class="text-center">
+            <span class="text-blue-400 text-3xl block">{{ value }}</span>
+            <span class="text-sm">{{ formatKey(key) }}</span>
+          </div>
+        </div>
+        <div class="space-y-4 text-black dark:text-white">
+          <p>
+            I completed all my formal education in Dortmund, recently finishing my
+            <strong class="text-amber-400">Bachelor’s Degree in Computer Science</strong> at TU Dortmund. Earlier, I
+            graduated from Gymnasium, earning my diploma with a solid foundation in multiple
+            disciplines. 
+          </p>
+          <p>
+            Alongside my academic pursuits, I’ve also made significant achievements in the realm of
+            sports. I hold several <strong class="text-amber-400">Swimming Licenses</strong> and a
+            <strong class="text-amber-400">Lifeguard Certification</strong>, showcasing my dedication to health and
+            fitness.
+          </p>
+          <p>
+            I'm multilingual, having earned certificates in various languages, including achieving a
+            <strong class="text-amber-400">C2 level in Serbian</strong>, which connects me to my cultural roots, and a
+            <strong class="text-amber-400">B2 level in Spanish</strong>, demonstrating my global perspective and
+            adaptability.
+          </p>
+        </div>
       </div>
     </div>
+
+    <Transition name="fade">
+      <h3
+        class="absolute right-4 xl:right-[20%] text-2xl font-bold text-center"
+        :key="currentPhoto.title"
+      >
+        {{ currentPhoto.title }}
+      </h3>
+    </Transition>
   </div>
-
-      <Transition name="fade">
-          <h3 class=" absolute right-4 xl:right-[20%] text-2xl font-bold text-center" :key="currentPhoto.title">
-              {{ currentPhoto.title }}
-            </h3>
-        </Transition>
-    </div>
-
 </template>
 
 <script setup>
@@ -61,16 +69,15 @@ import ReactCert from '@/assets/certReact.png';
 import { computed, ref } from 'vue';
 
 const photos = [
-  { title: 'React Course', src: ReactCert, alt: 'React Certificate' },
+  {
+    title: 'Swimming Licenses and LifeGuard',
+    src: 'https://www.sportwelt-dortmund.de/uploads/pics/HOMBRUCH.JPG',
+    alt: 'Swimming Pool'
+  },
   {
     title: 'Finished Gymnasium (2.7)',
     src: 'https://www.ruhrnachrichten.de/wp-content/uploads/2023/01/11/13/630_0900_2653689_WS_Hlg-1648x824.jpg',
     alt: 'German School'
-  },
-  {
-    title: 'Finished Bachelor in Computer Science (3.4)',
-    src: 'https://iw-up.com/wp-content/uploads/2013/09/logo-tu-dortmund.jpg',
-    alt: 'TU Dortmund Logo'
   },
   {
     title: 'Serbian Course until C2',
@@ -83,17 +90,19 @@ const photos = [
     alt: 'Spanish Flag'
   },
   {
-    title: 'Swimming Licenses and LifeGuard',
-    src: 'https://www.sportwelt-dortmund.de/uploads/pics/HOMBRUCH.JPG',
-    alt: 'Swimming Pool'
-  }
+    title: 'Finished Bachelor in Computer Science (3.3)',
+    src: 'https://iw-up.com/wp-content/uploads/2013/09/logo-tu-dortmund.jpg',
+    alt: 'TU Dortmund Logo'
+  },
+
+  { title: 'React Course', src: ReactCert, alt: 'React Certificate' }
 ]
 
 const stats = {
-  yearsOld: 38,
-  yearsWebDev: '10+',
+  finishedProjects: '10+',
+  yearsDev: '5+',
   amazingDogs: 1,
-  commits: 3149
+  commits: '1085+'
 }
 
 const currentIndex = ref(0)
