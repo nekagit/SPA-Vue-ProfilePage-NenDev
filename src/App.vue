8b881ad2034142f339a39b0283743c7d7794a20e
nekagit <nenad99@hotmail.de>
2024-12-18 15:48:02 +0100

    Update: removed intro icon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <!-- App.vue -->
 <template>
-  <LoadingScreen v-if="isLoading" />
-  <NavBar v-if="!isLoading" />
+  <NavBar />
   <transition name="fade" mode="out-in">
-    <router-view v-if="!isLoading" class="bg-white dark:bg-[#090E19] text-black dark:text-white" />
+    <router-view class="bg-white dark:bg-[#090E19] text-black dark:text-white" />
   </transition>
-  <NenFooter v-if="!isLoading" />
+  <NenFooter />
 </template>
 
 <script setup lang="ts">
-import LoadingScreen from '@/components/LoadingScreen.vue'
-import NenFooter from '@/components/NenDev/NenFooter.vue'
-import { onMounted, ref } from 'vue'
-import NavBar from './components/NenDev/NavBar.vue'
-
-const isLoading = ref(true)
-
-onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false // Hide loading screen after 3 seconds
-  }, 2800)
-})
+import NenFooter from '@/components/NenDev/NenFooter.vue';
+import NavBar from './components/NenDev/NavBar.vue';
 </script>
 
 <style>
